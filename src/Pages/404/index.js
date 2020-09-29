05de1d7fa0d96909993af122581eab96cf96f7ce
EdgarJFA <62776977+EdgarJFA@users.noreply.github.com>
2020-09-29 21:35:03 +0200

    Update NotFound Pages Sep 29

--- src/Pages/404/index.js
+++ src/Pages/404/index.js
@@ -1,29 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../../global.css';
 import '../../components/css/text.css';
 import './styles.css'
 
 function NotFound() {
-
-    const [time, setTime] = useState(30);
-    const History = useHistory();
-
-    function handleReloadTime(time) { 
-        if(time > 1) {             
-            setTime(time-1);
-        } else {
-            // Reload da pagina getCoutries
-            History.push('/listofcountries');
-        }
-    } 
-
-    useEffect(() => {
-        setTimeout(                
-            () => {handleReloadTime(time)}
-        , 1000);        
-    }, [time]);
 
     return (
         <div id="PageNotFound">
@@ -33,13 +15,12 @@ function NotFound() {
                     <Link to={{ pathname: "https://github.com/apilayer/restcountries" }} target="_blank" rel="noopener noreferrer">View on Github</Link>
                     <Link to={{ pathname: "https://restcountries.eu/#rest-countries" }} target="_blank" rel="noopener noreferrer">Docs API</Link>
                     <Link to="/listofcountries" rel="noopener noreferrer">Get Countries</Link>
-                    {/* <a href="http://" target="_blank" rel="noopener noreferrer">Contact US</a> */}
                 </header>
                 <main>
                     <div className="TextContent">
                         <h2>We have a problem</h2>
                         <p>Page not found</p>
-                        <Link to="/listofcountries" className="normal">Reload {(time<=0)? '': time}</Link>
+                        <Link to="/listofcountries" className="normal">Reload</Link>
                     </div>
                 </main>
             </div>
